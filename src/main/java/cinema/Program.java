ea444d60d75a31d21d8a214ebf5b3a03d7b4cc74
Ole Markus Roland <ole.markus.roland@no.experis.com>
2024-05-23 11:42:59 +0200

    Initial commit

---
+++ src/main/java/cinema/Program.java
@@ -0,0 +1,8 @@
+package main.java.cinema;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Cinema Updater is running...");
+        // Add your updating logic here
+    }
+}
